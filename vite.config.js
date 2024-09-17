140652f04046578a4dd77ca9b04dbc116ac7ec6e
sammy6378 <samuel202mwangi@gmail.com>
2024-09-17 12:27:19 +0300

    update vite config base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reactjs-multistep-form/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
